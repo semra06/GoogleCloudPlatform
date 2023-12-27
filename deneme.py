61299bbd1ee29a86e6a4792c3af4da6ea9bd9869
semra06 <108343622+semra06@users.noreply.github.com>
2023-12-27 14:56:26 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,1 +1,10 @@
-print ("Hello world")
+import requests
+
+url = 'http://localhost:8080'
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+    print('Response:', response.text)
+except requests.exceptions.RequestException as e:
+    print(f'Error: {e}')
